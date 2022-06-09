3ebff7e78b2c3a9b251c76b4dce06ef1317f8796
Mao <mauricio.vera.r7@gmail.com>
2022-06-09 13:57:53 -0400

    puntaje y tiempo adicional

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const score_id = document.querySelector("#score")
 const end_game_container = document.querySelector("#end-game-container")
 
 // variables globales
-let i = 10 // segundos restantes
+let segundos = 10 // segundos restantes
 let score = 0 // 
 let palabra; // palabra por adivinar
 
@@ -38,21 +38,68 @@ let tiempo = 10000 // 10 segundos
 // funcion randomWords
 function randomWords() {
   const palabraAzar = words[Math.floor(Math.random() * words.length)]
-  return palabraAzar;
+  return palabraAzar
 }
 
 function addToDOM() {
-  palabra = randomWords()
-  palabraAleatoria.innerHTML = palabra;
+  palabra = randomWords() // palabraAzar
+  palabraAleatoria.innerHTML = palabra // variable global
+  timeSpan_id.innerHTML = `${segundos}seg` // segundos
+  score_id.innerHTML = score
 }
-// inicio del juego
 addToDOM()
+
+// inicio del juego
+// funcion updateScore **************************************
+input.addEventListener("keyup", function (ev) {
+  if (ev.keyCode == 13) {  // evento input
+    if (input.value == palabra) {
+      
+      alert('match')
+      
+      //i = 10
+      // tiempo = 10000
+      // console.log(i)
+      score++ // score
+      segundos += 3
+      // random()
+      // return
+    } else {
+      console.log(input.value)
+      alert('erró')
+            
+    }
+    input.value = ''
+    addToDOM()
+  } else {
+    //i--
+  }  
+})
 // manipulación del tiempo
 
 const probando = setInterval(function () {
+  segundos--
+
+  //i--
   // se ejecuta cada 1 segundo
 }, 1000)
+/*
+
+const probando = setInterval(function () {
+  setInterval(function () {
+    console.log('tiempo excedido')
+    gameOver()
+  }, tiempo) //10 seguntos
+  i--  // desminuyendo 1 seg
+  timeSpan.textContent = i
+  if (i == 0) {
+    clearInterval(probando)
+  }
+}, 1000)
+*/
 // funcion actualizarTiempo
+
+
 
 // funcion gameOver
 function gameOver() {
@@ -61,33 +108,3 @@ function gameOver() {
   <p>puntaje final de la partida: ${j}</p>
   <button onclick="location.reload()">Volvé a empezar</button>`
 }
-
-// funcion updateScore
-
-// funcion addToDOM (llama a randomWords y agrega al DOM)
-
-
-
-
-// funcion updateScore **************************************
-text.addEventListener("keyup", function (ev) {
-  if (ev.keyCode == 13) {  // evento input
-    if (input.value == randomWord.value) {
-      text.value = ''
-      console.log(text.value)
-      alert('match')
-
-      i = 10
-      tiempo = 10000
-      console.log(i)
-      score.textContent = j++ // score
-      random()
-      // return
-    } else {
-      alert('erró')
-    }
-  } else {
-    i--
-  }
-
-})
